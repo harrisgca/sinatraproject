ca4b944beb974e473363aff50f7644aef890150b
Glenn Harris <harris.glenn.jr@gmail.com>
2014-09-05 02:16:50 -0700

    styling for main page

--- public/js/tab.js
+++ public/js/tab.js
@@ -1,11 +1,56 @@
 $(document).ready(function(){
 
-$('#reddit').html("<h1>Reddit placeholder</h1>");
-$('#flickr').html("<h1>Flickr placeholder</h1>");
-$('#twitter').html("<h1>Twitter placeholder</h1>");
 
-var idName = $('li.active').attr('id');
-console.log(idName);
+  var nbaHTML = '<ul>';
+  var nflHTML = '<ul>';
+  var soccerHTML = '<ul>';
 
+  var nbaCallback = function(response){
+    var nbaArr = response.data.children;
+
+    $.each(nbaArr, function(idx,val){
+      nbaHTML += '<li>';
+      nbaHTML += nbaArr[idx].data.title;
+      nbaHTML += '</li>';
+      nbaHTML += "<hr>";
+    });
+
+    nbaHTML += "</ul>";
+    
+    $('#nba').html(nbaHTML);
+  }; //end nbaCallback
+
+  var nflCallback = function(response){
+    var nflArr = response.data.children;
+
+    $.each(nflArr, function(idx,val){
+      nflHTML += '<li>';
+      nflHTML += nflArr[idx].data.title;
+      nflHTML += '</li>';
+      nflHTML += '<hr>';
+    });
+
+    nflHTML += "</ul>";
+    $('#nfl').html(nflHTML);
+  }; //end nflCallback
+
+  var soccerCallback = function(response){
+    var soccerArr = response.data.children;
+
+    $.each(soccerArr, function(idx,val){
+      soccerHTML += '<li>';
+      soccerHTML += soccerArr[idx].data.title;
+      soccerHTML += '</li>';
+      soccerHTML += '<hr>';
+    }); //end soccerCallback
+
+    soccerHTML += "</ul>";
+
+    $('#soccer').html(soccerHTML);
+  };
+
+  $.getJSON('http://www.reddit.com/r/nba/new/.json',{limit:7}, nbaCallback);
+  $.getJSON('http://www.reddit.com/r/nfl/new/.json',{limit:7}, nflCallback);
+  $.getJSON('http://www.reddit.com/r/soccer/new/.json',{limit:7}, soccerCallback);
 
 });
