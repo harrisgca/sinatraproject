d0887171e156556360d29ba1c249f7f9fa7e47aa
Glenn Harris <harris.glenn.jr@gmail.com>
2014-09-04 18:28:29 -0700

    first commit

---
+++ public/js/tab.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+$('#reddit').html("<h1>Reddit placeholder</h1>")
+$('#flickr').html("<h1>Flickr placeholder</h1>")
+$('#twitter').html("<h1>Twitter placeholder</h1>")
+
+
+});
