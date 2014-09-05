2111aa678e939218963e5f7ea98df0086c27ea3e
Glenn Harris <harris.glenn.jr@gmail.com>
2014-09-04 19:05:42 -0700

    final for day

--- public/js/tab.js
+++ public/js/tab.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 
-$('#reddit').html("<h1>Reddit placeholder</h1>")
-$('#flickr').html("<h1>Flickr placeholder</h1>")
-$('#twitter').html("<h1>Twitter placeholder</h1>")
+$('#reddit').html("<h1>Reddit placeholder</h1>");
+$('#flickr').html("<h1>Flickr placeholder</h1>");
+$('#twitter').html("<h1>Twitter placeholder</h1>");
+
+var idName = $('li.active').attr('id');
+console.log(idName);
 
 
 });
